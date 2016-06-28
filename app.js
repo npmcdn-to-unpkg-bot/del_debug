6f0e63ac6e8bec57ce04a916ab451d5b58a12f59
Zach <zach.falen@partnerscreative.com>
2016-06-28 12:22:37 -0600

    here we go again

--- app.js
+++ app.js
@@ -17,17 +17,17 @@ var session = require('express-session');
 var app = express();
 
 
-var mongoose = require('mongoose');
-var uriUtil = require('mongodb-uri');
-
-var options = {
-  server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
-  replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
-};
-
-var mongodbUri = process.env.MONGOLAB_URI || 'mongodb://localhost/delTest';
-var mongooseUri = uriUtil.formatMongoose(mongodbUri);
-mongoose.connect(mongooseUri, options);
+// var mongoose = require('mongoose');
+// var uriUtil = require('mongodb-uri');
+//
+// var options = {
+//   server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+//   replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
+// };
+//
+// var mongodbUri = process.env.MONGOLAB_URI || 'mongodb://localhost/delTest';
+// var mongooseUri = uriUtil.formatMongoose(mongodbUri);
+// mongoose.connect(mongooseUri, options);
 
 
 // Webpack config to enable hot reloading

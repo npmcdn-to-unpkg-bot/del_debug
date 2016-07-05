11ce594a42400144d19dba85f0c3651a525a3e83
Zach Falen <zach.falen@partnerscreative.com>
2016-07-05 12:36:42 -0600

    Fixed last question bug - now immediately shows selected answer, reflecs in the store.

--- CLIENTSIDE/components/survey.js
+++ CLIENTSIDE/components/survey.js
@@ -13,6 +13,19 @@ var SurveyOptions = React.createClass({
     TweenMax.to($(e.target).closest('.optionBox'), .5, {backgroundColor: 'rgba(32,32,32,.45)', color: '#A1EB87'});
   },
 
+  checkEnd: function(answer){
+    if (this.props.questionIndex < this.props.totalQuestions) {
+      console.log('submitting')
+      this.props.handleNext(answer)
+    } else {
+      console.log('submitting last question')
+
+      this.props.handleNext(answer)
+
+      this.forceUpdate();
+    }
+  },
+
   render: function(){
 
     $('#choiceA').removeClass('selectedAnswer')
@@ -39,10 +52,10 @@ var SurveyOptions = React.createClass({
     return(
       <div className="selectorHolder">
         <p className='backBtn' onClick={this.props.handleBack}><i className='fa fa-arrow-circle-left'></i> back</p>
-        <div id='choiceA' className='optionBox'  onMouseOver={this.handleHoverOver} onMouseOut={this.handleHoverOut} onClick={() => this.props.handleNext(true)}>
+        <div id='choiceA' className='optionBox'  onMouseOver={this.handleHoverOver} onMouseOut={this.handleHoverOut} onClick={() => this.checkEnd(true)}>
           <p className='temp2'>{formattedChoiceA}</p>
         </div>
-        <div id='choiceB' className='optionBox right' onMouseOver={this.handleHoverOver} onMouseOut={this.handleHoverOut} onClick={() => this.props.handleNext(false)}>
+        <div id='choiceB' className='optionBox right' onMouseOver={this.handleHoverOver} onMouseOut={this.handleHoverOut} onClick={() => this.checkEnd(false)}>
           <p className='temp2'>{formattedChoiceB}</p>
         </div>
         <div id='divider' className='temp'><p>or</p></div>
@@ -91,25 +104,39 @@ var Survey = React.createClass({
 
   handleNext: function(answer){
 
-    this.state.responses["question" + this.state.question].answer = answer;
+      this.state.responses["question" + this.state.question].answer = answer;
 
-    if (this.state.question < (this.state.data.length - 1) ){
+    if (this.state.question < (this.state.data.length - 1 ) ){
+
       this.setState({
         question: this.state.question + 1
       })
-    }
 
-    var index = this.state.question + 1
-    if (this.state.responses["question" + index].answer != null){
-        console.log('Question ' + index + ' has been answered already')
-        this.setState({
-          hasBeenAnswered: true
-        })
+
+      var index = this.state.question + 1
+      if (this.state.responses["question" + index].answer != null){
+          console.log('Question ' + index + ' has been answered already')
+          this.setState({
+            hasBeenAnswered: true
+          })
+        } else {
+          this.setState({
+            hasBeenAnswered: false
+          })
+        }
       } else {
-        this.setState({
-          hasBeenAnswered: false
-        })
-      }
+        var index = this.state.question
+        if (this.state.responses["question" + index].answer != null){
+            console.log('Question ' + index + ' has been answered already')
+            this.setState({
+              hasBeenAnswered: true
+            })
+          } else {
+            this.setState({
+              hasBeenAnswered: false
+            })
+          }
+        }
     },
 
   handleBack: function(){
@@ -141,7 +168,7 @@ var Survey = React.createClass({
   render: function(){
 
     var questionIndex = this.state.question;
-    var SurveyOptionsCond = this.state.gotData ? <SurveyOptions choiceA={this.state.data[questionIndex].acf.passion_choice_a} choiceB={this.state.data[questionIndex].acf.passion_choice_b} hasBeenAnswered={this.state.hasBeenAnswered} answer={this.state.responses["question" + questionIndex].answer} handleNext={this.handleNext} handleBack={this.handleBack} handleReport={this.handleReport}/> : <div>nodata</div>
+    var SurveyOptionsCond = this.state.gotData ? <SurveyOptions choiceA={this.state.data[questionIndex].acf.passion_choice_a} choiceB={this.state.data[questionIndex].acf.passion_choice_b} questionIndex={questionIndex} totalQuestions={this.state.data.length - 1} hasBeenAnswered={this.state.hasBeenAnswered} answer={this.state.responses["question" + questionIndex].answer} handleNext={this.handleNext} handleBack={this.handleBack} handleReport={this.handleReport}/> : <div>nodata</div>
 
     return(
       <div>

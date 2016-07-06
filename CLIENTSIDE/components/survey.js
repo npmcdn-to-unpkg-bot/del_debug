136942e2816f2a402481afa438bc780aa722ed4a
Zach Falen <zach.falen@partnerscreative.com>
2016-07-06 11:47:41 -0600

    cleaning up extraneous files

--- CLIENTSIDE/components/survey.js
+++ CLIENTSIDE/components/survey.js
@@ -2,6 +2,53 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var classNames = require( 'classnames' );
 
+function getSharedStates(){
+  return({
+    gotData: false,
+    data: [],
+    responses: {}
+  })
+};
+
+function getInitialData(url, scope){
+  $.ajax({
+     url: url,
+     type: 'GET',
+     dataType: 'json',
+     success: function(res) {
+
+       // Set up our client-side "store" for this section's user responses
+       var tempData = {};
+       for (var i = 1; i < res.length; i++){
+         tempData["question" + (i)] = {answer: null}
+       }
+
+       scope.setState({
+         gotData: true,
+         data: res,
+         responses: tempData
+       })
+     }
+   })
+};
+
+function convertToScore(url, scope, data){
+   $.ajax({
+      url: url,
+      type: 'POST',
+      data: data,
+      success: function(res) {
+        console.log('success! Got results!')
+        scope.setState({
+          score: res
+        })
+      },
+      error: function(xhr, status, err){
+              console.log('Can\'t do that, Tiger!')
+              console.error(status, err.toString)
+          }.bind(this)
+       });
+ };
 
 var ScoreDisplay = React.createClass({
   render: function(){
@@ -38,30 +85,28 @@ var SurveyOptions = React.createClass({
   },
 
   checkEnd: function(answer){
+
+    // Just tell us if it's the last question in this section
     if (this.props.questionIndex < this.props.totalQuestions) {
       this.props.handleNext(answer)
     } else {
       console.log('submitting last question')
-
       this.props.handleNext(answer)
     }
   },
 
   render: function(){
 
+    // Reset the response indicator
     $('#choiceA').removeClass('selectedAnswer')
     $('#choiceB').removeClass('selectedAnswer')
 
-    var choiceA= this.props.choiceA
-    var formattedChoiceA = choiceA.replace(/(<([^>]+)>)/ig,"").toUpperCase()
+    // Scrub out the WordPress HTML tags && make uppercase
+    function formatQuestion(input){
+      return input.replace(/(<([^>]+)>)/ig,"").toUpperCase()
+    }
 
-    var choiceB= this.props.choiceB
-    var formattedChoiceB = choiceB.replace(/(<([^>]+)>)/ig,"").toUpperCase()
-
-
-    var temp = this.props.answer ? <p>"A"</p> : <p>"B"</p>
-    var test = this.props.hasBeenAnswered ? <h4>YOU ANSWERED <strong style={{display: 'inline-block'}}>{temp}</strong></h4> : <p>not answered</p>
-
+    // Highlight the user's selection
     if (this.props.answer != null) {
       if (this.props.answer) {
         $('#choiceA').addClass('selectedAnswer')
@@ -70,6 +115,9 @@ var SurveyOptions = React.createClass({
       }
     }
 
+    // Temporary indicator for debugging
+    var answer = this.props.answer ? <p>"A"</p> : <p>"B"</p>
+    var responseIndicator = this.props.hasBeenAnswered ? <h4>YOU ANSWERED <strong style={{display: 'inline-block'}}>{answer}</strong></h4> : <p>not answered</p>
 
     return(
       <div>
@@ -77,13 +125,13 @@ var SurveyOptions = React.createClass({
           <h3 className='passionTitle'>Which would you prefer or find most rewarding?</h3>
           <p className='backBtn' onClick={this.props.handleBack}><i className='fa fa-arrow-circle-left'></i> back</p>
           <div id='choiceA' className='optionBox'  onMouseOver={this.handleHoverOver} onMouseOut={this.handleHoverOut} onClick={() => this.checkEnd(true)}>
-            <p className='temp2'>{formattedChoiceA}</p>
+            <p className='temp2'>{formatQuestion(this.props.choiceA)}</p>
           </div>
           <div id='choiceB' className='optionBox right' onMouseOver={this.handleHoverOver} onMouseOut={this.handleHoverOut} onClick={() => this.checkEnd(false)}>
-            <p className='temp2'>{formattedChoiceB}</p>
+            <p className='temp2'>{formatQuestion(this.props.choiceB)}</p>
           </div>
           <div id='divider' className='temp'><p>or</p></div>
-          {test}
+          {responseIndicator}
           <ScoreDisplay score={this.props.score}/>
         </div>
       </div>
@@ -94,51 +142,35 @@ var SurveyOptions = React.createClass({
 var Survey = React.createClass({
 
   getInitialState: function(){
-    return({
-      gotData: false,
-      data: [],
+
+    // Set up any states unique to THIS component
+    var uniqueStates = {
       question: 1,
-      responses: {},
       hasBeenAnswered: false,
       score: {}
-    })
+    }
+
+    // jQuery merge objects
+    var stateObject = $.extend({}, uniqueStates, getSharedStates())
+    return(stateObject)
   },
 
   componentDidMount: function(){
+    var scope = this;
+    var url = "https://deloitteeyf.wpengine.com/wp-json/wp/v2/passionq?_embed&filter[posts_per_page]=999&filter[orderby]=menu_order&filter[order]=ASC";
 
-    var that = this;
-
-    $.ajax({
-       url: "https://deloitteeyf.wpengine.com/wp-json/wp/v2/passionq?_embed&filter[posts_per_page]=999&filter[orderby]=menu_order&filter[order]=ASC",
-       type: 'GET',
-       dataType: 'json',
-       success: function(res) {
-         var tempData = {};
-
-         for (var i = 1; i < res.length; i++){
-           tempData["question" + (i)] = {answer: null}
-         }
-
-         that.setState({
-           gotData: true,
-           data: res,
-           responses: tempData
-         })
-       }
-    });
+    getInitialData(url, scope);
   },
 
   handleNext: function(answer){
 
-      this.state.responses["question" + this.state.question].answer = answer;
+    // Log the user's response
+    this.state.responses["question" + this.state.question].answer = answer;
 
-    if (this.state.question < (this.state.data.length - 1 ) ){
+    // NOT on the last question...
+    if (this.state.question < (this.state.data.length - 1) ){
 
-      this.setState({
-        question: this.state.question + 1
-      })
-
-
+      // First, tell us if user has already answered the question
       var index = this.state.question + 1
       if (this.state.responses["question" + index].answer != null){
           this.setState({
@@ -149,6 +181,13 @@ var Survey = React.createClass({
             hasBeenAnswered: false
           })
         }
+
+        // Then, increment the question index
+        this.setState({
+          question: this.state.question + 1
+        })
+
+        // ON the last question....
       } else {
         var index = this.state.question
         if (this.state.responses["question" + index].answer != null){
@@ -160,29 +199,18 @@ var Survey = React.createClass({
               hasBeenAnswered: false
             })
           }
-        }
+      };
 
-        var submitData = {}
-        for (var i = 1; i < 9; i++){
-            submitData['question' + i] = this.state.responses['question' + i].answer
-        }
+      // Format response data for sending into server
+      var responseData = {}
+      for (var i = 1; i < 9; i++){
+          responseData['question' + i] = this.state.responses['question' + i].answer
+      }
 
-        var that = this;
-        $.ajax({
-           url: "/api/survey",
-           type: 'POST',
-           data: submitData,
-           success: function(res) {
-             console.log('success! Got results!')
-             that.setState({
-               score: res
-             })
-           },
-           error: function(xhr, status, err){
-                   console.log('Can\'t do that, Tiger!')
-                   console.error(status, err.toString)
-               }.bind(this)
-        });
+      var scope = this;
+      var url = "/api/survey";
+
+      convertToScore(url, scope, responseData);
     },
 
   handleBack: function(){
